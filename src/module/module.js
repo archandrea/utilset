2bc6f29137ee517c1df6ae2dd3572f7c2f54857d
xz <luc_xz@foxmail.com>
2023-06-25 17:04:45 +0800

    firstcommit

---
+++ src/module/module.js
@@ -0,0 +1,13 @@
+/**
+ * @description: 用于批量导入的工具函数
+ * @param {Function} reqCtx 传入require.context(path,subdir,regexp,mode)
+ * @return {Object} {filename:module,filename:module,filename:module...} 可以直接分解在vue的components中
+ * @author: xz
+ */
+function importDir(reqCtx) {
+  return reqCtx.keys().reduce((modules, path) => {
+    const name = path.match(/\/([^/]+)\./)[1]
+    modules[name] = reqCtx(path).default
+    return modules
+  }, {})
+}
