86f32c6056b51270f4139ccbcde3e4294550946f
xz <luc_xz@foxmail.com>
2023-06-26 09:19:24 +0800

    add new fns

---
+++ src/array.js
@@ -0,0 +1,38 @@
+/**
+ * @description: 根据给定的阶梯给数组分级（目前只支持正序），比如：rankArray([0,10,100,200,10000], [1,100,200], 'a') 得到[[0],[10],[100],[200,10000]]
+ * 也可以按某个属性分级，比如：rankArray([{a:10},{a:20},{a:30},{a:99},{a:100},], [1,100,200], 'a') 得到[[0],[10],[100],[200,10000]]
+ * @param {Array} data 需要分级的数据
+ * @param {Array} steps 阶梯（分级的数据项数为阶梯数+1，阶梯的每项包含在大的那部分）
+ * @param {String} prop（可选）按该属性分级，未填则按data的项本身分级
+ * @return {Array} 分级后的数据
+ * @author: xz
+ */
+function rankArray(data, steps, prop) {
+  if (!data || !steps) {
+    return []
+  }
+  let rankedList = []
+  let startIdx = 0
+  let endIdx
+  let lastIdx = data.length - 1
+  steps.forEach((step, i, arr) => {
+    let idx = data.findIndex((item) => {
+      return prop ? (item[prop] >= step) : (item >= step)
+    })
+    let rank
+    if (idx === -1 && (prop ? data[lastIdx][prop] : data[lastIdx]) < arr[i - 1]) {
+      rank = []
+    } else {
+      if (idx === -1) {
+        endIdx = lastIdx + 1
+      } else {
+        endIdx = idx
+      }
+      rank = data.slice(startIdx, endIdx)
+      startIdx = endIdx
+    }
+    rankedList.push(rank)
+  })
+  rankedList.push(data.slice(endIdx))
+  return rankedList
+}
---
+++ src/digit.js
@@ -0,0 +1,11 @@
+/**
+ * @description: 将数字格式化为每三位加“,”的格式，例如：1,000,000.00
+ * @param {Number/String} n 需要格式化的数字，如果不是数字会自动转化
+ * @return {*} 格式化后的字符串
+ * @author: xz
+ */
+function formatNumberWithCommas(n) {
+  const parts = Number(n).toString().split('.')
+  parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+  return parts.join('.')
+}
